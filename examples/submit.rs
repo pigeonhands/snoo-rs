29a0d3e7ad04759405f46161af0b9f9d2a005300
Sam M <sam.mackenzie@live.com>
2020-05-17 22:37:36 +1200

    updated example.

--- examples/submit.rs
+++ examples/submit.rs
@@ -7,10 +7,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let r = Reddit::new_script("snoo-rs", env!("REDDIT_PASSWORD"), env!("REDDIT_ID"), env!("REDDIT_SECRET")).await?;
 
     let sr = r.subreddit("test");
-    //let new_post_data = sr.submit("test from snoo-rs", SubredditSubmission::Link("https://github.com/pigeonhands/snoo-rs")).await?;
-   // println!("{:?}", new_post_data);
+    let new_post_data = sr.submit("test from snoo-rs", SubredditSubmission::Link("https://github.com/pigeonhands/snoo-rs")).await?;
+    println!("{:?}", new_post_data);
 
-    let new_post = r.submission_from_link("https://www.reddit.com/r/test/comments/glccw4/test_from_snoors/").await?;
+    let new_post = r.submission_from_link(&new_post_data.url).await?;
     println!("{:?}", new_post.op().title());
     for comment in new_post.comments() {
         println!("\t{}", comment.body());
